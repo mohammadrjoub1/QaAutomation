ff1ce601552aa145af7dc1d0ecb12b28792f540f
mohammad rjoub <122231941+mohammadrjoub1@users.noreply.github.com>
2023-10-30 11:54:01 +0200

    Update orangeHRM.cy.ts

--- cypress/e2e/orangeHRM.cy.ts
+++ cypress/e2e/orangeHRM.cy.ts
@@ -2,7 +2,7 @@ import { Login } from "../support/helpers/login";
 import { faker } from "@faker-js/faker";
 
 describe("seneario#1", () => {
-  beforeEach("Will Occur Befor Every Test", () => {
+  beforeEach("Will happen Beforw Every Test", () => {
     cy.visit("/auth/login").then(() => {
       Login.login("Admin", "admin123"); // Perform the login
     });
