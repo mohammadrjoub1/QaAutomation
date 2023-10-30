535b621f1101b4ba5cd0cdc078d7f7fff4c445db
Your Name <you@example.com>
2023-10-30 11:50:47 +0200

    master branch

--- cypress/e2e/orangeHRM.cy.ts
+++ cypress/e2e/orangeHRM.cy.ts
@@ -1,7 +1,7 @@
 ﻿import { Login } from "../support/helpers/login";
 import { faker } from "@faker-js/faker";
 
-describe("testing", () => {
+describe("seneario#1", () => {
   beforeEach("Will Occur Befor Every Test", () => {
     cy.visit("/auth/login").then(() => {
       Login.login("Admin", "admin123"); // Perform the login
