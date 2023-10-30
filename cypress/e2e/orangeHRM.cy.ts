2609b47505b90e303b79a6bb9fa92e4d997c3e56
Your Name <you@example.com>
2023-10-30 08:46:11 +0200

    decribe and it content

--- cypress/e2e/orangeHRM.cy.ts
+++ cypress/e2e/orangeHRM.cy.ts
@@ -1,12 +1,12 @@
 ﻿import { Login } from "../support/helpers/login";
 
-describe("testing posts in Hrm", () => {
+describe("testing", () => {
   beforeEach("Will Occur Befor Every Test", () => {
     cy.visit("/auth/login").then(() => {
       Login.login("Admin", "admin123"); // Perform the login
     });
   });
 
-  it("testing the post function in the Buzz Area", () => {
+  it("testing ", () => {
   });
 });
