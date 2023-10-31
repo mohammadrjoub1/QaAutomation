694e18bff1593cede7e692fe2db547cb7873b516
Your Name <you@example.com>
2023-11-01 01:28:12 +0200

    efited

--- cypress/e2e/orangeHRM.cy.ts
+++ cypress/e2e/orangeHRM.cy.ts
@@ -1,12 +1,11 @@
-﻿import loginAsAdmin from "../support/pageObjects/loginAsAdmin";
-import { faker } from "@faker-js/faker";
+﻿import { faker } from "@faker-js/faker";
 import { createEmployeeViaApi } from "../support/helpers/createEmployeeViaApi";
 import { loginWithEmployeeViaUi } from "../support/helpers/loginWithEmployeeViaUi";
 import { addCandidate } from "../support/helpers/addCandidate";
+import { loginAsAdmin } from "../support/pageObjects/loginAsAdmin";
 
 describe("senario #1", () => {
   beforeEach(() => {
-    cy.visit("/auth/login");
     loginAsAdmin.loginAdmin();
   });
 
@@ -14,25 +13,14 @@ describe("senario #1", () => {
     const username = faker.internet.userName();
     cy.fixture("employeeData").as("empData");
     cy.get("@empData").then((data: any) => {
-      createEmployeeViaApi.addEmployeeViaApi(
-        data.empID,
-        data.firstName,
-        data.lastName,
-        data.middleName,
-        username,
-        data.password,
-      );
+      createEmployeeViaApi.addEmployeeViaApi(data.empID, data.firstName, data.lastName, data.middleName, username, data.password);
       loginWithEmployeeViaUi.loginViaUi(username, data.password);
     });
   });
 
   it("create a candidate Then upload file and make the assertion", () => {
     cy.visit("/recruitment/addCandidate");
-    addCandidate.createNewCandidate(
-      faker.person.firstName(),
-      faker.person.middleName(),
-      faker.person.lastName(),
-    );
+    addCandidate.createNewCandidate(faker.person.firstName(), faker.person.middleName(), faker.person.lastName());
     addCandidate.uploadFileWithAssertion("cypress/fixtures/RJOUB123.pdf");
   });
 });
